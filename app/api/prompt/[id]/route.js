b80857daa31e75ed44c09d1622df72427f59ee14
James Gaulhofer <jg@devcircle.at>
2024-02-11 18:34:59 +0100

    fix: delete prompt not updating db

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -21,14 +21,12 @@ export const PATCH = async (request, { params }) => {
     try {
         await connectToDB();
 
-        // Find the existing prompt by ID
         const existingPrompt = await Prompt.findById(params.id);
 
         if (!existingPrompt) {
             return new Response("Prompt not found", { status: 404 });
         }
 
-        // Update the prompt with new data
         existingPrompt.prompt = prompt;
         existingPrompt.tag = tag;
 
@@ -43,10 +41,7 @@ export const PATCH = async (request, { params }) => {
 export const DELETE = async (request, { params }) => {
     try {
         await connectToDB();
-
-        // Find the prompt by ID and remove it
-        await Prompt.findByIdAndRemove(params.id);
-
+        await Prompt.findByIdAndDelete(params.id);
         return new Response("Prompt deleted successfully", { status: 200 });
     } catch (error) {
         return new Response("Error deleting prompt", { status: 500 });
